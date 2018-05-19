e36efa22761703ceaa39b7f49586e39a609c8bcb
Luiz Avila <lapavila@gmail.com>
2018-05-19 14:17:51 -0300

    Implementando Autorizacao

--- src/main/java/org/gujavasc/meuslivros/api/controller/LivroController.java
+++ src/main/java/org/gujavasc/meuslivros/api/controller/LivroController.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,6 +28,7 @@ public class LivroController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LIVRO') and #oauth2.hasScope('write')")
     public ResponseEntity<Livro> save(@RequestBody LivroDto dto) {
         Livro livro = mapper.map(dto, Livro.class);
         livro = service.save(livro);
