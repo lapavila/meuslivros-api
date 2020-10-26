c36f0da9ea5af107309275dfd12f5808fe357285
Luiz Avila <lapavila@gmail.com>
2020-10-26 20:52:02 -0300

    Migracao para o java 11

--- src/main/resources/db/migration/V1_0__cria_schema_inicial.sql
+++ src/main/resources/db/migration/V1_0__cria_schema_inicial.sql
@@ -1,32 +1,32 @@
 CREATE TABLE categoria (
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(200) NOT NULL,
-);
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(200) NOT NULL
+)  ENGINE=INNODB;
 
 CREATE TABLE livro (
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  titulo VARCHAR(200) NOT NULL,
-  autor VARCHAR(200) NOT NULL,
-	categoria_id bigint NOT NULL,
-	CONSTRAINT livro_categoria_fk FOREIGN KEY (categoria_id) REFERENCES categoria(id)
-);
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(200) NOT NULL,
+    autor VARCHAR(200) NOT NULL,
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+)  ENGINE=INNODB;
 
 CREATE TABLE usuario (
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(200) NOT NULL,
   username VARCHAR(20) NOT NULL,
   password VARCHAR(200) NOT NULL
-);
+)  ENGINE=INNODB;
 
 CREATE TABLE permissao (
-	id BIGINT(20) PRIMARY KEY,
+	id INT PRIMARY KEY,
 	nome VARCHAR(100) NOT NULL,
 	descricao VARCHAR(50)
-);
+)  ENGINE=INNODB;
 
 CREATE TABLE usuario_permissao (
-	usuario_id BIGINT(20) NOT NULL,
-	permissao_id BIGINT(20) NOT NULL,
-	CONSTRAINT usuario_permissao_fk FOREIGN KEY (usuario_id) REFERENCES usuario(id),
-	CONSTRAINT permissao_usuario_fk FOREIGN KEY (permissao_id) REFERENCES permissao(id)
-);
+	usuario_id INT NOT NULL,
+	permissao_id INT NOT NULL,
+	FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+	FOREIGN KEY (permissao_id) REFERENCES permissao(id)
+)  ENGINE=INNODB;
